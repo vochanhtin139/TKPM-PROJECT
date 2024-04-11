2f5e69e5e7030e6c3fef2965cbb354c25933c821
Quỳnh Phan <phanquynh437@gmail.com>
2024-04-11 23:17:49 +0700

    update home page

--- src/source/public/js/header.js
+++
@@ -1,8 +0,0 @@
-const searchInput = document.querySelector('.header .search-input');
-searchInput.addEventListener('focus', function () {
-  searchInput.placeholder = '';
-});
-
-searchInput.addEventListener('blur', function () {
-  searchInput.placeholder = 'Search...';
-});
